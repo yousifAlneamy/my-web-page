fff5dfbeec60a8405f29a31bbcf051d007539600
Yousif Alneamy <yousif.aladdin@gmail.com>
2017-01-19 14:18:06 +0100

    Delete persons-list.component.js

--- angular/app/profiles/persons-list.component.js
+++
@@ -1,6 +0,0 @@
-angular.module('app.personsList')
-.component('personsList', {
-    templateUrl: 'app/profiles/persons-list.component.html',
-    controller: personsController,
-    controllerAs: 'ctrl',
-});
